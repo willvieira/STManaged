3ed09cc1d8256ef4c4ba8018272d6b98351466a0
Willian Vieira <willian.vieira@usherbrooke.ca>
2018-05-16 11:18:34 +0200

    add climate change in the model

--- R/run.R
+++ R/run.R
@@ -3,11 +3,14 @@
   # - steps (nb of steps; step * 5 = years)
   # - initLand (output of the `create_landscape` function)
   # - params (parameters)
+  # - Forest management intensity [0-1]
+  # - RCP (three options of climate change: RCP = 2.6, RCP = 4.5, RCP = 6, RCP = 8.5)
  ## Output:
   # - lands (a list for every step)
   # - Each step (or list) of lands have a list of state occupancy and the respective env1 vector
  ## Extra functions:
   # - neighbor_prop (calculates the proportion between states for each neigbor area - 9 cells)
+  # - cc_diff (get the params difference between env1 before and after climate change)
 
 # function to return neighborhood proportion considering the 8 neighbors
 neighbor_prop <- function(neighbor) {
@@ -18,12 +21,51 @@ neighbor_prop <- function(neighbor) {
   return(setNames(c(B, T, M), c("B", "T", "M")))
 }
 
+# function to define the temperature increase over the time steps based in RCP scenarios
+cc_diff <- function(env1, # pars as a list for each row of the lanscape
+                    RCP) # RCP either 0, 2.6, 4.5, 6 and 8.5
+{
+  load('data/scale_info.robj')
+
+  # unscale temperature to add climate change
+  tempSc0 <- env1
+  tempUn0 <- tempSc0 * vars.sd['annual_mean_temp'] + vars.means['annual_mean_temp']
+
+  # add climate change
+  if(RCP == 2.6) tempUn1 <- tempUn0 + 1 # increase of 1 degree
+  if(RCP == 4.5) tempUn1 <- tempUn0 + 1.8 # 1.8 degrees
+  if(RCP == 6) tempUn1 <- tempUn0 + 2.2 # 1.8 degrees
+  if(RCP == 8.5) tempUn1 <- tempUn0 + 3.7 # 1.8 degrees
+  if(RCP == 0) tempUn1 <- tempUn0
+
+  # scale future temperature
+  tempSc1 <- (tempUn1 - vars.means['annual_mean_temp'])/vars.sd['annual_mean_temp']
+
+  # list of parameters for each row cell before and after climate change (temperature gradient)
+  pars0 <- lapply(as.list(tempSc0), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
+  pars1 <- lapply(as.list(tempSc1), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
+
+  # list of difference between before and after climate change
+  parsDiff <- mapply("-", pars1, pars0, SIMPLIFY = FALSE)
+
+  rer <- setNames(list(pars0, parsDiff), c('pars0', 'parsDiff'))
+
+  return(rer)
+}
+
 # Main function to run the model over time
-run_model <- function(steps, initLand, params)
+run_model <- function(steps, initLand, params,
+                      plantInt = 0,
+                      harvInt = 0,
+                      thinInt = 0,
+                      enrichInt = 0,
+                      RCP = 0)
 {
 
-  # list of parameters for each row cell (temperature gradient)
-  pars <- lapply(as.list(initLand[['env1']]), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
+  # climate change (define a list of parameters difference between before and after climate change)
+  pars0Diff <- cc_diff(initLand[['env1']], RCP = RCP)
+  # divide the difference by time steps - 20
+  pars0Diff[['parsInc']] <- lapply(pars0Diff[['parsDiff']], function(x) x/(20))
 
   # lands
   lands <- setNames(list(initLand), 'landT0')
@@ -32,6 +74,15 @@ run_model <- function(steps, initLand, params)
   for(i in 1:steps) {
 
     landStep <- land0
+
+    # get param with climate change
+    if(i == 1) {
+      pars <- pars0Diff[['pars0']] # first time step with original env1
+    }else if(i > 1 && i < 20) { # climate change increasing from 2:20 steps (= 100 years)
+      pars <- mapply("+", pars, pars0Diff[['parsInc']], SIMPLIFY = FALSE)
+    }else { # keep climate stable for last steps
+      pars <- pars
+    }
 
     for(c in 2:(length(land0)-1)) {
       for(r in 2:(length(land0[[1]])-1)) {
@@ -43,7 +94,7 @@ run_model <- function(steps, initLand, params)
         y0 <- neighbor_prop(neighbor)
 
         # run the model
-        y1 <- model_fm(t = 1, y0, params = pars[[r]], plantInt = 0, harvInt = 0, thinInt = 0, enrichInt = 0)
+        y1 <- model_fm(t = 1, y0, params = pars[[r]], plantInt, harvInt, thinInt, enrichInt)
         y1 <- y0 + unlist(y1) # update cell
         landStep[[c]][r] <- names(y1)[which(y1 == max(y1))] # update landStep
 
--- main.R
+++ main.R
@@ -9,8 +9,13 @@ initLand <- create_landscape(climRange = c(-1.9, 0.6), cellSize = 20)
 # load pars
 params = read.table("data/pars.txt", row.names = 1)
 
-# run the model
-lands <- run_model(steps = 200, initLand, params) # 1000 years = 7MB of data; 42 seconds
+# run the model (1000 years = 7MB of data; 42 seconds)
+lands <- run_model(steps = 250, initLand, params,
+                   plantInt = 0, # for all management practice: [0-1]
+                   harvInt = 0,
+                   thinInt = 0,
+                   enrichInt = 0,
+                   RCP = 4.5) # either 0, 2.6, 4.5, 6.0 and 8.5
 
 # outputs
   ## plot some landscapes
@@ -18,7 +23,7 @@ lands <- run_model(steps = 200, initLand, params) # 1000 years = 7MB of data; 42
   invisible(sapply(1:9, function(x) plot_landscape(lands[[x]], title = names(lands)[x])))
 
   ## create a gif (11 seconds for 21 images; 8Kbs)
-  make_gif(lands, steps = 10, fps = 5)
+  make_gif(lands, steps = 10, fps = 5, gifName = 'RCP2.6')
 
   ## plot
   par(mfrow = c(1, 2))
