1c91d3c3cb913ea5c274e156606aee8efa8e9222
Willian Vieira <willian.vieira@usherbrooke.ca>
2018-05-17 21:58:41 +0200

    add function to get range limit of each state

---
+++ R/range_limit.R
@@ -0,0 +1,35 @@
+# Function to get range limit
+  ## Input:
+   # - land (one step landscape)
+   # - occup (degree of occupancy at the row level to be considered a limit)
+  ## Output:
+   # - nb of the row (nb for both Boreal and Temperate states)
+
+range_limit <- function(land, occup)
+{
+  nc <- length(land[[1]])
+  nr <- length(land[[1]][[1]])
+
+  # list to matrix
+  landM <- matrix(unlist(land[[1]]), ncol = nc)
+
+  # summary for each row (get proportion for each row of the landscape)
+  getProp <- function(x) {
+    B <- sum(landM[x,] == "B")/nc
+    T <- sum(landM[x,] == "T")/nc
+    M <- sum(landM[x,] == "M")/nc
+    R <- 1 - sum(B, T, M)
+    return(setNames(c(B, T, M, R), c('B', 'T', 'M', 'R')))
+  }
+
+  prop <- sapply(1:nr, getProp)
+
+  # limit Boreal
+  limB <- as.numeric(max(which(prop['B',] > occup)))
+  # limit Temperate
+  limT <- as.numeric(min(which(prop['T',] > occup)))
+
+  rtrn <- setNames(c(limB, limT), c('limitB', 'limitT'))
+
+  return(rtrn)
+}
