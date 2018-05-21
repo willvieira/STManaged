4c928a5018ce55224495682a02da20a93a75d38f
Willian Vieira <willian.vieira@usherbrooke.ca>
2018-05-21 13:25:21 +0000

    add different options of climate change growth (beta version)

---
+++ R/climate_change.R
@@ -0,0 +1,92 @@
+# - cc_diff (get the params difference between env1 before and after climate change)
+
+# function to define the temperature increase over the time steps based in RCP scenarios
+clim_diff <- function(env1, # pars as a list for each row of the lanscape
+                      RCP = 0, # RCP either 0, 2.6, 4.5, 6 and 8.5
+                      params)
+{
+  load('data/scale_info.robj')
+
+  # unscale temperature to add climate change
+  tempSc0 <- env1
+  tempUn0 <- tempSc0 * vars.sd['annual_mean_temp'] + vars.means['annual_mean_temp']
+
+  # add climate change
+  if(RCP == 2.6) tempUn1 <- tempUn0 + 1 # increase of 1 degree
+  if(RCP == 4.5) tempUn1 <- tempUn0 + 1.8 # 1.8 degrees
+  if(RCP == 6) tempUn1 <- tempUn0 + 2.2 # 1.8 degrees
+  if(RCP == 8.5) tempUn1 <- tempUn0 + 3.7 # 1.8 degrees
+  if(RCP == 0) tempUn1 <- tempUn0
+
+  # test if RCP was corectly insert
+  if(!exists('tempUn1')) stop("'RCP' must be either '0', '2.6', '4.5', '6' or '8.5'")
+
+  # scale future temperature
+  tempSc1 <- setNames((tempUn1 - vars.means['annual_mean_temp'])/vars.sd['annual_mean_temp'], '')
+
+  # list of parameters for each row cell before and after climate change (temperature gradient)
+  pars0 <- lapply(as.list(tempSc0), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
+  pars1 <- lapply(as.list(tempSc1), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
+
+  # tansform to matrix
+  pars0M <- matrix(unlist(pars0), nr = 9)
+  pars1M <- matrix(unlist(pars1), nr = 9)
+
+  # names
+  row.names(pars0M) <- names(pars0[[1]])
+  row.names(pars1M) <- names(pars0[[1]])
+
+  # list of difference between before and after climate change
+  parsDiff <- mapply("-", pars1, pars0)
+
+  climDiff <- setNames(list(pars0M, pars1M, parsDiff), c('pars0', 'pars1', 'parsDiff'))
+
+  return(climDiff)
+}
+
+# Function to create the parameter vector depending on the growth type of climate change
+clim_increase <- function(steps, climDiff, growth = 'linear') {
+
+  if(growth == 'linear') {
+
+    # define the increasing parameters (20 because = 100 years of climate change)
+    # formula: 1:20 * parsDiff/20 + pars0
+      # first part of formula: returns a list of 20 years (each year with one matrix of parameters)
+      parsDiff20_120 <- sapply(1:20, function(x) (climDiff[['parsDiff']]/20) * x, simplify = FALSE)
+      # second part: sum each matrix 'parsDiff/20' with 'pars0'
+      parsDiff20_pars0 <- lapply(parsDiff20_120, function(x) x + climDiff[['pars0']])
+
+      # list with parameter increasing
+      # starting with pars0, increasing from 1 to 20 and then fixed until nb of steps
+      parsCC <- c(list(climDiff[['pars0']]), parsDiff20_pars0, rep(list(parsDiff20_pars0[[20]]), steps))
+
+  }else if(growth == 'exponential') {
+
+    # furmula: pars0 * [(pars1/pars0)^(1/20*x)]
+      # first part: return list with 20 years
+      pars1_pars0 <- sapply(1:20, function(x) (climDiff[['pars1']]/climDiff[['pars0']])^(0.05*x), simplify = FALSE)
+
+      # second part: pars0 * pars1_pars0
+      pars0_pars1_pars0 <- lapply(pars1_pars0, function(x) climDiff[['pars0']] * x)
+
+      # list with parameter increasing
+      # starting with pars0, increasing from 1 to 20 and then fixed until nb of steps
+      parsCC <- c(list(climDiff[['pars0']]), pars0_pars1_pars0, rep(list(pars0_pars1_pars0[[20]]), steps))
+
+  }
+
+  return(parsCC)
+}
+
+
+ff <- function(x = 1:20, a, b) {
+   ex <- a * ((b/a)^(1/length(x)*x))
+   return(ex)
+}
+
+ff2 <- function(x = 1:20, a, b) {
+   ex <- a * ((b*1.2/a)^(1/length(x)*x))
+   return(ex)
+}
+plot(1:20, ff(1:20, a = 0.2, b = 0.9), type = 'l')
+points(1:20, ff2(1:20, a = 0.2, b = 0.9), type = 'l', col = 2)
--- R/run.R
+++ R/run.R
@@ -10,7 +10,6 @@
   # - Each step (or list) of land have a list of state occupancy and the respective env1 vector
  ## Extra functions:
   # - neighbor_prop (calculates the proportion between states for each neigbor area - 9 cells)
-  # - cc_diff (get the params difference between env1 before and after climate change)
 
 # function to return neighborhood proportion considering the 8 neighbors
 neighbor_prop <- function(neighbor) {
@@ -19,39 +18,6 @@ neighbor_prop <- function(neighbor) {
   M = sum(neighbor == "M")/9
 
   return(setNames(c(B, T, M), c("B", "T", "M")))
-}
-
-# function to define the temperature increase over the time steps based in RCP scenarios
-cc_diff <- function(env1, # pars as a list for each row of the lanscape
-                    RCP, # RCP either 0, 2.6, 4.5, 6 and 8.5
-                    params)
-{
-  load('data/scale_info.robj')
-
-  # unscale temperature to add climate change
-  tempSc0 <- env1
-  tempUn0 <- tempSc0 * vars.sd['annual_mean_temp'] + vars.means['annual_mean_temp']
-
-  # add climate change
-  if(RCP == 2.6) tempUn1 <- tempUn0 + 1 # increase of 1 degree
-  if(RCP == 4.5) tempUn1 <- tempUn0 + 1.8 # 1.8 degrees
-  if(RCP == 6) tempUn1 <- tempUn0 + 2.2 # 1.8 degrees
-  if(RCP == 8.5) tempUn1 <- tempUn0 + 3.7 # 1.8 degrees
-  if(RCP == 0) tempUn1 <- tempUn0
-
-  # scale future temperature
-  tempSc1 <- (tempUn1 - vars.means['annual_mean_temp'])/vars.sd['annual_mean_temp']
-
-  # list of parameters for each row cell before and after climate change (temperature gradient)
-  pars0 <- lapply(as.list(tempSc0), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
-  pars1 <- lapply(as.list(tempSc1), function(x) get_pars(ENV1 = x, ENV2 = 0, params, int = 5))
-
-  # list of difference between before and after climate change
-  parsDiff <- mapply("-", pars1, pars0, SIMPLIFY = FALSE)
-
-  rer <- setNames(list(pars0, parsDiff), c('pars0', 'parsDiff'))
-
-  return(rer)
 }
 
 # Main function to run the model over time
@@ -65,10 +31,9 @@ run_model <- function(steps, initLand,
                       saveOutput = F)
 {
 
-  # climate change (define a list of parameters difference between before and after climate change)
-  pars0Diff <- cc_diff(initLand[['env1']], RCP = RCP, params)
-  # divide the difference by time steps - 20
-  pars0Diff[['parsInc']] <- lapply(pars0Diff[['parsDiff']], function(x) x/(20))
+  # climate change
+  climDiff <- clim_diff(initLand[['env1']], RCP = RCP, params)
+  pars <- clim_increase(steps = steps, climDiff, growth = 'linear')
 
   # lands
   lands <- setNames(list(initLand), 'land_T0')
@@ -77,15 +42,6 @@ run_model <- function(steps, initLand,
   for(i in 1:steps) {
 
     landStep <- land0
-
-    # get param with climate change
-    if(i == 1) {
-      pars <- pars0Diff[['pars0']] # first time step with original env1
-    }else if(i > 1 && i < 20) { # climate change increasing from 2:20 steps (= 100 years)
-      pars <- mapply("+", pars, pars0Diff[['parsInc']], SIMPLIFY = FALSE)
-    }else { # keep climate stable for last steps
-      pars <- pars
-    }
 
     for(c in 2:(length(land0)-1)) {
       for(r in 2:(length(land0[[1]])-1)) {
@@ -97,7 +53,7 @@ run_model <- function(steps, initLand,
         y0 <- neighbor_prop(neighbor)
 
         # run the model
-        y1 <- model_fm(t = 1, y0, params = pars[[r]], plantInt, harvInt, thinInt, enrichInt)
+        y1 <- model_fm(t = 1, y0, params = pars[[i]][,r], plantInt, harvInt, thinInt, enrichInt)
         y1 <- y0 + unlist(y1) # update cell
         y1['R'] <- 1 - sum(y1)
 
@@ -115,7 +71,7 @@ run_model <- function(steps, initLand,
     lands[[paste0('land_T', i)]] <- landStep # save the land i step
 
     # print progress
-    cat("====>", format(100*i/steps, digits = 4), "%", "\r")
+    cat("==>", format(100*i/steps, digits = 4), "%", "\r")
   }
   # add steps, management and RCP information
   lands[['steps']] <- steps
