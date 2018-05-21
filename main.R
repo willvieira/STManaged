fd617cf86e6c5f1edfa073b6332d04560d2a42ce
Willian Vieira <willian.vieira@usherbrooke.ca>
2018-05-21 13:26:40 +0000

    update landscape size and add another way to viz migration

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ file.sources <- dir('R/')
 invisible(sapply(paste0('R/', file.sources), source))
 
 ## get initial lanscape
-initLand <- create_landscape(climRange = c(-1.9, 0.6), cellSize = 5)
+initLand <- create_landscape(climRange = c(-2.1, 0.45), cellSize = 1)
 plot_landscape(initLand, Title = 'initial_landscape')
 
 # load pars
@@ -44,10 +44,30 @@ load('lands.Rdata')
 
   ## Migration
   par(mar = c(3,3,1.5,0.8), mgp = c(1.5, 0.3, 0), tck = -.008, xaxs='i', yaxs='i')
-  plot(c(0, 200), c(0, 200), type = 'l', xlab = 'limit of B', ylab = 'limit of T')
+  plot(c(0, 1200), c(0, 1200), type = 'l', xlab = 'limit of B', ylab = 'limit of T')
   for(i in seq(0.3, 0.9, 0.1)) {
     mig <- lapply(lands[grep('land', names(lands))], function(x) range_limit(x, occup = i))
     mig <- setNames(data.frame(matrix(unlist(mig), nc = 2, byrow = T)), c('limitB', 'limitT'))
-    points(mig, xlim = c(0, 200), ylim = c(0, 200), col = i*10, pch = 20)
+    points(mig, xlim = c(0, 200), ylim = c(0, 1200), col = i*10, pch = 20)
   }
   legend('bottomright', legend = seq(0.3, 0.9, 0.1), pch = 20, col = seq(0.3, 0.9, 0.1)*10, bty = 'n')
+
+  ## Migration 2
+  mig <- lapply(lands[grep('land', names(lands))], function(x) range_limit(x, occup = 0.7))
+  mig <- setNames(data.frame(matrix(unlist(mig), nc = 2, byrow = T)), c('limitB', 'limitT'))
+  par(mar = c(3,3,1.5,0.8), mgp = c(1.5, 0.3, 0), tck = -.008, xaxs='i', yaxs='i')
+  plot(1:dim(mig)[1], mig[, 1], type = 'l', ylim = c(1200, 0), xlab = 'Time (year * 5)', ylab = 'Range limit (latitudinal gradient)', col = 'darkcyan')
+  lines(mig[, 2], col = 'orange')
+  abline(v = 20, lty = 2); #mtext('Climate change stops', side = 3, at = 20)
+  legend('topright', legend = c('Boreal', 'Temperate'), lty = 1, col = c('darkcyan', 'orange'), bty = 'n')
+
+  ## plot parameters increase
+  climDiff <- clim_diff(env1 = initLand[['env1']], RCP = 8.5, params)
+  climIncLin <- clim_increase(steps = 100, climDiff, growth = 'linear')
+  climInc <- clim_increase(steps = 100, climDiff, growth = 'exponential')
+
+  par(mfrow = c(3, 3))
+  for(i in 1:9) {
+    plot(1:121, unlist(lapply(climInc, function(x) x[i, 100])), type = 'l', xlab = 'steps', ylab = 'parameter')
+    points(1:121, unlist(lapply(climIncLin, function(x) x[i, 100])), type = 'l', xlab = 'steps', ylab = 'parameter', col = 2)
+  }
