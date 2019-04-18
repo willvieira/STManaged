363ba5b1e912fab1e750a2f8c8a721e354d869ca
Willian Vieira <willian.vieira@usherbrooke.ca>
2019-04-18 15:39:58 -0400

    reorganize functions

--- R/range_limit.R
+++ R/range_limit.R
@@ -5,15 +5,6 @@
   ## Output:
    # - nb of the row (nb for both Boreal and Temperate states)
 
-# summary for each row (get proportion for each row of the landscape)
-getProp <- function(x, nRow) {
- B <- sum(x == 1)/nRow
- T <- sum(x == 2)/nRow
- M <- sum(x == 3)/nRow
- R <- 1 - sum(B, T, M)
- return(setNames(c(B, T, M, R), c('B', 'T', 'M', 'R')))
-}
-
 range_limit <- function(land, nRow, nCol, occup)
 {
   # list to matrix
