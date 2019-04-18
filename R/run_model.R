9cb622dfe51dba4a04c1a7dccb4d10e02bcf6c93
Willian Vieira <willian.vieira@usherbrooke.ca>
2019-04-18 00:58:06 -0400

    merge run_model and run_modelParallel in the same function

--- R/run_model.R
+++ R/run_model.R
@@ -11,19 +11,15 @@
  ## Extra functions:
   # - neighbor_prop (calculates the proportion between states for each neigbor area - 9 cells)
 
-# function to return neighborhood proportion considering the 8 neighbors
-neighbor_prop <- function(neighbor) {
-  return(c(B = sum(neighbor == 1), T = sum(neighbor == 2), M = sum(neighbor == 3))/9)
-}
-
 # Main function to run the model over time
-run_model <- function(steps, initLand,
+run_model <- function(steps,
+                      initLand,
                       managInt = c(0, 0, 0, 0), # for plant, harv, thin and enrich
                       RCP = 0,
                       stoch = T,
+                      cores = 1,
                       outputLand = NA, # if NA, everything is out, otherwise specify a vector of timeSteps values [1 - steps]
-                      saveRangeLimit = FALSE,
-                      occup = 0.75,
+                      rangeLimitOccup = NULL,
                       saveOutput = F,
                       fileOutput = NULL, # name of the file
                       folderOutput = NULL) # name of the output file, if NULL will just save in the mail `output` folder
@@ -46,38 +42,59 @@ run_model <- function(steps, initLand,
   parCell = parCell[((nCol - 2) * 2 + 1):length(parCell)]
   states = 1:4
 
-  # range limit table (divided by nCol so I can compare with different landcape cell size)
-  rangeLimitDF = data.frame(step = 1:(steps + 1), limitB = numeric(steps + 1), limitT = numeric(steps + 1))
-  rangeLimitDF[1, 2:3] = range_limit(land0, nRow = nRow, nCol = nCol, occup = occup)/nCol
+  if(cores > 1) {
+    # create a border vector with states from land0 to be added at each time step (the border will not be updated
+    indexToAdd = which(!(1:(nCol * nRow) %in% position))
+    border = land0[indexToAdd]
+  }
+
+  if(!is.null(rangeLimitOccup)) {
+    # range limit table (divided by nCol so I can compare with different landcape cell size)
+    rangeLimitDF = data.frame(step = 1:(steps + 1), limitB = numeric(steps + 1), limitT = numeric(steps + 1))
+    rangeLimitDF[1, 2:3] = range_limit(land0, nRow = nRow, nCol = nCol, occup = rangeLimitOccup)/nCol
+  }
+
+  if(cores == 1) land1 <- land0
 
   for(i in 1:steps) {
 
-    land1 <- land0
+    # two run options (non-parallel and parallel)
+    if(cores == 1) {
+      for(cell in 1:length(neighbor)) {
+        # get neighborhood
+        y0 <- neighbor_prop(land0[neighbor[[cell]]])
+        # run the model
+        y1 <- model_fm(t = 1, y0, params = pars[[i]][, parCell[cell]], managInt)
+        y1 <- y0 + unlist(y1) # update cell
+        y1['R'] <- 1 - sum(y1)
 
-    for(cell in 1:length(neighbor)) {
-
-      # get neighborhood
-      y0 <- neighbor_prop(land0[neighbor[[cell]]])
-      # run the model
-      y1 <- model_fm(t = 1, y0, params = pars[[i]][, parCell[cell]], managInt)
-      y1 <- y0 + unlist(y1) # update cell
-      y1['R'] <- 1 - sum(y1)
-
-      if(stoch == T) {
-        land1[neighbor[[cell]][5]] <- states[rmultinom(n = 1, size = 1, prob = y1) == 1] # get a state depending on the probability `p`
-      }else {
-        land1[neighbor[[cell]][5]] <- states[y1 == max(y1)] # update landStep
+        if(stoch == T) {
+          land1[neighbor[[cell]][5]] <- states[rmultinom(n = 1, size = 1, prob = y1) == 1] # get a state depending on the probability `p`
+        }else {
+          land1[neighbor[[cell]][5]] <- states[y1 == max(y1)] # update landStep
+        }
       }
+    }else if(cores > 1) {
+      # run for all cells
+      land = parallel::mcmapply(function(cell) cellRun(cell, neighbor, land0, pars, parCell, i, managInt, stoch, nCol), seq_along(neighbor), mc.cores = cores)
+
+      land1 = setNames(land, position)
+
+      # add border to keep same size at each time step
+      land1 = c(land1, border)
+      land1 = land1[match(1:length(land1), as.numeric(names(land1)))] # sort to keep same order
+    }else{
+      stop("cores must be a numeric greater than zero")
     }
 
     # calculate range limit
-    rangeLimitDF[i + 1, 2:3] = range_limit(land1, nRow = nRow, nCol = nCol, occup = occup)/nCol
+    if(!is.null(rangeLimitOccup)) rangeLimitDF[i + 1, 2:3] = range_limit(land1, nRow = nRow, nCol = nCol, occup = rangeLimitOccup)/nCol
 
     land0 <- land1 # update land0 for next time step
     lands[[paste0('land_T', i)]] <- land1 # save land time step
 
     # print progress
-    cat("==>", format(100*i/steps, digits = 4), "%", "\r")
+    cat("==>", format(100*i/steps, digits = 2), "%", "\r")
   }
 
   # keep all output lands or just a part of it?
@@ -93,7 +110,7 @@ run_model <- function(steps, initLand,
   lands[['RCP']] <- RCP
   lands[['nCol']] <- nCol
   lands[['nRow']] <- nRow
-  lands[['rangeLimit']] <- rangeLimitDF
+  if(!is.null(rangeLimitOccup)) lands[['rangeLimit']] <- rangeLimitDF
 
   # save or simply return the output
   if(saveOutput == TRUE) {
@@ -115,4 +132,24 @@ run_model <- function(steps, initLand,
   }else {
     return(lands)
   }
+}
+
+# function to return neighborhood proportion considering the 8 neighbors
+neighbor_prop <- function(neighbor) {
+  return(c(B = sum(neighbor == 1), T = sum(neighbor == 2), M = sum(neighbor == 3))/9)
+}
+
+cellRun <- function(cell, neighbor, land0, pars, parCell, i, managInt, stoch, nCol) {
+  states = 1:4
+  y0 <- neighbor_prop(land0[neighbor[[cell]]])
+  y1 <- model_fm(t = 1, y = y0, params = pars[[i]][, parCell[cell]], managInt)
+  y1 <- y0 + unlist(y1) # update cell
+  y1['R'] <- 1 - sum(y1)
+  if(stoch == T) {
+    cell <- states[rmultinom(n = 1, size = 1, prob = y1) == 1] # get a state depending on the probability `p`
+  }else {
+    cell <- states[y1 == max(y1)] # update cell
+  }
+
+  return(cell)
 }
