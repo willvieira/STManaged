440b3a7450ec88492274ba5a7cb60ddfc8c2b76b
Willian Vieira <willian.vieira@usherbrooke.ca>
2019-04-23 17:17:48 -0400

    new add argument in plot_occupancy

--- R/plot_occupancy.R
+++ R/plot_occupancy.R
@@ -2,42 +2,44 @@
 #'
 #' This function plots the distribution of state occupancy over the Latitudinal gradient of the landscape
 #' @param lands the object output of the \code{\link{run_model}} function
-#' @param steps vector, time steps to be plotted. Remember to set \code{mfrow} before the function in case steps has more than one step
+#' @param step vector, time step to be plotted
 #' @param spar numeric, value between 0 and 1 to sooth the state occupancy
+#' @param states vector, the forest states to be ploted. 1 for Boreal, 2 for Temperate, 3 for Mixed and 4 for regeneration
+#' @param add logical, if \code{TRUE} the lines will be added to an existent plot
 #' @export
 #' @examples
 #' \dontrun{
-#' par(mfrorw = c(1, 2))
-#' plot_occupancy(lands, steps = c(1, 100), spar = 0.2)
+#' plot_occupancy(lands, step = 1, spar = 0.2, states = 1:4)
 #' }
 
-plot_occupancy <- function(lands, steps, spar)
+plot_occupancy <- function(lands, step, spar, states = 1:3, add = FALSE)
 {
   # define coordinates
   nCol <- lands[['nCol']]
   nRow <- lands[['nRow']]
 
   # checks
-  if(!all(steps %in% 0:lands[['steps']])) stop('steps vector must be included in 1:steps')
+  if(!all(step %in% 0:lands[['steps']])) stop('steps vector must be included in 1:steps')
+  if(!all(states %in% 1:4)) stop('states must an integer of at least one forest states values [1:4]')
 
-  for(i in steps) {
+  # get step name
+  stepName <- paste0('land_T', step)
 
-    # get step name
-    stepName <- paste0('land_T', i)
+  # vector to matrix
+  land <- matrix(lands[[stepName]], ncol = nRow)
 
-    # vector to matrix
-    land <- matrix(lands[[stepName]], ncol = nRow)
+  prop <- apply(land, 1, getProp, nRow = nRow)
 
-    prop <- apply(land, 1, getProp, nRow = nRow)
+  # plot
+  cols <- c("darkcyan", "orange", "palegreen3", "black")
+  stateNames <- c('Boreal', 'Temperate', 'Mixed', 'Regeneration')
 
-    # plot
-    cols <- c("darkcyan", "orange", "palegreen3", "black")
-
-    par(mar = c(2.5, 2.5, 3, 0.5), mgp = c(1.5, 0.3, 0), tck = -.008)
-    plot(1:nCol, prop[1,], pch = '', lwd = 1.3, xlab = "Latitudinal gradient", ylim = c(0, 1), ylab = "State occupancy", col = cols[1])
-    invisible(sapply(1:3, function(x) points(1:nCol, if(spar == 0) {prop[x, ]} else {smooth.spline(prop[x, ], spar = spar)$y}, type = 'l', lwd = 1.3, col = cols[x])))
-    legend(nRow/2 - nRow*.1, 0.8, legend = c('Boreal', 'Temperate', 'Mixed'), lty = 1, col = cols[1:3], bty = 'n', cex = 0.8)
-    mtext(stepName, 3, line = 0.5)
-
+  par(mar = c(2.5, 2.5, 3, 0.5), mgp = c(1.5, 0.3, 0), tck = -.008)
+  if(add == FALSE) {
+    plot(1:nCol, prop[1, ], pch = '', lwd = 1.3, xlab = "Latitudinal gradient", ylim = c(0, 1), ylab = "State occupancy", col = cols[1])
   }
+  invisible(sapply(states, function(x) points(1:nCol, if(spar == 0) {prop[x, ]} else {smooth.spline(prop[x, ], spar = spar)$y}, type = 'l', lwd = 1.3, col = cols[x])))
+  legend('topleft', legend = stateNames[states], lty = 1, col = cols[states], bg = 'white', cex = 0.8)
+  mtext(stepName, 3, line = 0.5)
+
 }
